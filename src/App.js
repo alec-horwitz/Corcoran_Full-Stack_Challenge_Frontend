0b27df7daa8164617d16a4d7f5ed11272e5f6040
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-11-12 13:22:59 -0500

    Init rails app with react-semantic-ui and implemented a simple semantic sortable table

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import _ from 'lodash'
+import { Table, Container, Button } from 'semantic-ui-react'
+import UUID from 'uuid';
+
+class App extends Component {
+
+  state = {
+    column: null,
+    data: tableData,
+    direction: null,
+  }
+
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending',
+      })
+
+      return
+    }
+
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending',
+    })
+  }
+
+  render() {
+    const { column, data, direction } = this.state
+
+    return (
+      <Table sortable celled fixed>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell
+              sorted={column === 'name' ? direction : null}
+              onClick={this.handleSort('name')}
+            >
+              Name
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={column === 'age' ? direction : null}
+              onClick={this.handleSort('age')}
+            >
+              Age
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={column === 'gender' ? direction : null}
+              onClick={this.handleSort('gender')}
+            >
+              Gender
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {_.map(data, ({ age, gender, name }) => (
+            <Table.Row key={name}>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{age}</Table.Cell>
+              <Table.Cell>{gender}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    )
+  }
+}
+
+export default App;
